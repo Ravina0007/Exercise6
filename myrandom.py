8247110c4dbab1c0295fc2ce7b5f4b08196f2c48
Ravina <raniravina71996@gmail.com>
2024-01-25 10:14:05 +0200

    Initial commit

---
+++ myrandom.py
@@ -0,0 +1,10 @@
+import random
+random_integer1 = random.randint(0, 9)
+random_integer2 = random.randint(0, 9)
+random_integer3 = random.randint(0, 9)
+
+print(random_integer1)
+print(random_integer2)
+print(random_integer3)
+
+(f'The code for the suitcase is{random_integer1}{random_integer2}{random_integer3}')
